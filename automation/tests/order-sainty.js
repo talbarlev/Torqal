d2111f06f87866170ced2c5e6e23b900d51fab16
talb <tal.bar-lev@moveo.it>
2025-05-20 19:27:39 +0300

    strcuture

--- automation/tests/order-sainty.js
+++ automation/tests/order-sainty.js
@@ -1,9 +1,11 @@
 import { DISCVOERY_TABS_OPTION_ENGLISH } from "../data/constant-option.js";
-import DiscoveryHomePage from "../pages/discovery-page.js";
-import ResturantPage from "../pages/resturant-page.js";
+import DiscoveryHomePage from "../pages/order-options/discovery-page.js";
+import ResturantPage from "../pages/order-options/resturant-page.js";
 
 describe('Orders [user not logged in]', function () {
     it(`Order [Resturant] -  One item from first resturant on page`, async function () {
+        const amountOfItems = 1;
+        let price;
 
         await DiscoveryHomePage.open()
 
@@ -14,11 +16,20 @@ describe('Orders [user not logged in]', function () {
         await ResturantPage.allProductsList.chooseElementFromListByIndex(4);
 
         // // Widget ? make selection 
-        // ProductsCatalogPage.selectItem();
+        await ItemsPage.clickOnPlusForItemByIndex(1);
 
-        // SelectOptionWidget.addOptions();
+        await SelectionPopUp.tickChoices();
 
-        // ProductCatalogPage.goToCart();
+        price = await SelectionPopUp.getPrice();
+
+        // wait to disapear
+        await SelectionPopUp.addToOrder();
+
+        const dataFromShowItems = await ItemsPage.getDataFromShowItems();
+        
+        expect(dataFromShowItems.price).equals(price)
+        expect(dataFromShowItems.amount).equals(amountOfItems)
+
 
 
     });
