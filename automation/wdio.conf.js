7ad1fb81e7012771f88acde3d1232ce434ce5988
talb <tal.bar-lev@moveo.it>
2025-05-20 17:55:42 +0300

    hooks

--- automation/wdio.conf.js
+++ automation/wdio.conf.js
@@ -2,7 +2,7 @@ import customCommands from "./custom-commands.js";
 
 export const config = {
     runner: 'local',
-    specs: ['./tests/order-sainty.js'],
+    discovery: ['./tests/order-sainty.js'],
     maxInstances: 1,
     capabilities: [{
         browserName: 'chrome',
@@ -27,5 +27,21 @@ export const config = {
 
         await browser.setWindowSize(1024, 768);
 
+    },
+
+    async beforeTest(test) {
+        Logger.info(`--- Running test ${test.title}`);
+
+    },
+
+    async afterTest(test, error, result, duration, passed, retries) {
+        if (result.passed === true)
+            Logger.info("Test passed! 😀 😃 😄");
+
+        else
+            await Logger.failWithScreenShot("Test failed!!! 😕", test);
     }
+
+
+
 };
